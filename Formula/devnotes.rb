a5cf85f3896421884030fe28fe8aaef854611e23
Harshit Pareek <hpareek1996@outlook.com>
2025-11-23 22:58:45 +0530

    Bump version to 1.1.3 and update URLs and SHA256

--- Formula/devnotes.rb
+++ Formula/devnotes.rb
@@ -1,26 +1,26 @@
 class Devnotes < Formula
   desc "Developer-focused note-taking app"
   homepage "https://github.com/Hpareek07/devnotes"
-  version "1.1.0"
+  version "1.1.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Hpareek07/homebrew-devnotes/releases/download/v1.1.0/devnotes-v1.1.0-darwin-arm64.tar.gz"
-      sha256 "235128c2b6ef4a805fbc54c104db0c3bf8f066cf23ae528ca886c0832594dea3"
+      url "https://github.com/Hpareek07/homebrew-devnotes/releases/download/v1.1.3/devnotes-v1.1.3-darwin-arm64.tar.gz"
+      sha256 "a01ce34d110d915bc43b05ef7fa4e9af0e7cf9d0b37b4f49e805833c1a241245"
     else
-      url "https://github.com/Hpareek07/homebrew-devnotes/releases/download/v1.1.0/devnotes-v1.1.0-darwin-amd64.tar.gz"
-      sha256 "a7bc278a61a887cfaeb04545f1462b719bd2d0628fbaa01da0866642e1e0c749"
+      url "https://github.com/Hpareek07/homebrew-devnotes/releases/download/v1.1.3/devnotes-v1.1.3-darwin-amd64.tar.gz"
+      sha256 "41e3787a98eb2d61aaec1aee746775bf6f17dda3f1cc83575b6d8df6d3841ab3"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/Hpareek07/homebrew-devnotes/releases/download/v1.1.0/devnotes-v1.1.0-linux-arm64.tar.gz"
-      sha256 "e413576868c10991ee4f68ac7e22479249c7d4dad26b808163933eb859d2bbc0"
+      url "https://github.com/Hpareek07/homebrew-devnotes/releases/download/v1.1.3/devnotes-v1.1.3-linux-arm64.tar.gz"
+      sha256 "4cd0a841e7095a53a94cb13b0c3eed002a3fd336e165232c01a3429f9cd19e0b"
     else
-      url "https://github.com/Hpareek07/homebrew-devnotes/releases/download/v1.1.0/devnotes-v1.1.0-linux-amd64.tar.gz"
-      sha256 "8dabf4c7a33229fd2e8bc7a54063b2430c9f165f4298bf84a48d977e9a2b6ec4"
+      url "https://github.com/Hpareek07/homebrew-devnotes/releases/download/v1.1.3/devnotes-v1.1.3-linux-amd64.tar.gz"
+      sha256 "55188bc6a675ee15ffb7d87604b808515bf9130bb11b663617924922feb5e39a"
     end
   end
 
